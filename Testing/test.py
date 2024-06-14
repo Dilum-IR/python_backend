efcc321e78a91ec37a690e98adcc1901ddcdc16f
Dilum-IR <rdinduwara19158@gmail.com>
2024-06-15 01:47:21 +0530

    python with firebase authentication using jwt and tesing

---
+++ Testing/test.py
@@ -0,0 +1,39 @@
+import requests
+
+token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRmOGIxNTFiY2Q5MGQ1YjMwMjBlNTNhMzYyZTRiMzA3NTYzMzdhNjEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZWNob2x5bmstY2YzY2EiLCJhdWQiOiJlY2hvbHluay1jZjNjYSIsImF1dGhfdGltZSI6MTcxODM5NDU2MywidXNlcl9pZCI6Ik9peXpGanR6bXFYV1hOb1FpZkh2UjIzdzJrQzMiLCJzdWIiOiJPaXl6Rmp0em1xWFdYTm9RaWZIdlIyM3cya0MzIiwiaWF0IjoxNzE4Mzk0NTYzLCJleHAiOjE3MTgzOTgxNjMsImVtYWlsIjoic3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJzdHJpbmdAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.Tctsc0LQtyOLpLAlMjR01ypUXtg8u1Rtqal49GVCtM3XtodXzWR78x51SsIhuQndtR9k5r2e9Jiq7w-eetQoeCt8S6U3KpZS9copBCRmELJIVSj9bd6P02hsZnNrl6ax50VUqBDsbH-YL6F6bg6K4zisP6Qzl1sgD-xvXRUzi6Twy3S68pB1E-MvaLprPj-_OqSrp2xyFW8-FK87RNA5vPNgm1jkGgnhut8mqHit0fqcThk4r0TTcFiMPuPNhyQTfhVs8OeJAGAhjtdvg6IhVFP75BTnaxAeerg6sxwzmomvMldXpsouWY1JW9BICUe8LAmugIshhyOgr8luQa0VCg"
+
+# test the jwt token data
+def test_endpoint():
+
+    headers = {
+        "authorization":token
+    }
+
+    res = requests.post(
+        "http://127.0.0.1:9000/ping",
+        headers=headers
+    )
+
+    return res.text
+
+print(test_endpoint())
+
+
+{
+"iss":"https://securetoken.google.com/echolynk-cf3ca",
+"aud":"echolynk-cf3ca",
+"auth_time":1718394563,
+"user_id":"OiyzFjtzmqXWXNoQifHvR23w2kC3",
+"sub":"OiyzFjtzmqXWXNoQifHvR23w2kC3",
+"iat":1718394563,
+"exp":1718398163,
+"email":"string@gmail.com",
+"email_verified":false,
+"firebase":{
+    "identities":{
+         "email":["string@gmail.com"]
+         },
+    "sign_in_provider":"password"
+    },
+"uid":"OiyzFjtzmqXWXNoQifHvR23w2kC3"
+}
--- main.py
+++ main.py
@@ -1,15 +1,89 @@
+import pyrebase
+import firebase_admin
+from firebase_admin import credentials,auth
 from typing import Union
-
 from fastapi import FastAPI
+import uvicorn
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from fastapi.requests import Request
+
+
+from Models.models import LoginSchema,SignUpSchema
+
 
 app = FastAPI()
 
+if not firebase_admin._apps:
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
+
+
+firebaseConfig = {
+  "apiKey": "AIzaSyDEdwe8Wqep4SDmTDe2Ld3t75X8Y_rpnP0",
+  "authDomain": "echolynk-cf3ca.firebaseapp.com",
+  "projectId": "echolynk-cf3ca",
+  "storageBucket": "echolynk-cf3ca.appspot.com",
+  "messagingSenderId": "403359480178",
+  "appId": "1:403359480178:web:adc7a005a98e0f15bdc5eb",
+  "measurementId": "G-6HD5F25FMJ",
+  "databaseURL":""
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{id}")
+def read_item(id: int, q: Union[str, None] = None):
+    return {"item_id": id, "q": q}
+
+
+@app.post("/signup")
+async def signup(user_data:SignUpSchema):
+
+    try:
+        user = auth.create_user(email=user_data.email,password=user_data.password)
+
+        return JSONResponse(content={
+            "message":f"Usesr account created user id is {user.uid}"
+        },status_code=200,)
+    except auth.EmailAlreadyExistsError:
+        raise HTTPException(status_code=400,detail=f"Account already created for the {user_data.email}")
+
+
+@app.post("/login")
+async def creatae_access_token(user_data:LoginSchema):
+
+    try:
+        user = firebase.auth().sign_in_with_email_and_password(email=user_data.email,password=user_data.password)
+
+        token = user['idToken']
+
+        return JSONResponse(content={
+            "token":f"User account token is {token}"
+        },status_code=200,)
+    except auth.EmailAlreadyExistsError:
+        raise HTTPException(status_code=400,detail=f"Invalid credintials")
+    
+@app.post("/ping")
+async def validate_token(request:Request):
+
+    headers = request.headers
+    jwt = headers.get("authorization")
+
+    try:
+        verify_user = auth.verify_id_token(jwt)
+
+        return JSONResponse(content={
+            "token":verify_user
+        },status_code=200,)
+    except auth.EmailAlreadyExistsError:
+        raise HTTPException(status_code=400,detail=f"Invalid authorization")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="127.0.0.1",port=9000,reload=True)
