903cf3b5557276f70c3aec8083fe05a38f4052c2
Dilum-IR <rdinduwara19158@gmail.com>
2024-11-26 22:58:47 +0530

    changed

--- api/main.py
+++ api/main.py
@@ -31,17 +31,6 @@ app.add_middleware(
 )
 load_dotenv()
 
-# firebaseConfig = {
-#   "apiKey": "AIzaSyDEdwe8Wqep4SDmTDe2Ld3t75X8Y_rpnP0",
-#   "authDomain": "echolynk-cf3ca.firebaseapp.com",
-#   "projectId": "echolynk-cf3ca",
-#   "storageBucket": "echolynk-cf3ca.appspot.com",
-#   "messagingSenderId": "403359480178",
-#   "appId": "1:403359480178:web:adc7a005a98e0f15bdc5eb",
-#   "measurementId": "G-6HD5F25FMJ",
-#   "databaseURL":""
-# }
-
 # firebase = pyrebase.initialize_app(firebaseConfig)
 
 @app.get("/")
